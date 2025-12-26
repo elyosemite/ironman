3c849c65e1502f12d682a38bf828b484698062da
Yuri Melo <yurifullstack@gmail.com>
2025-12-26 12:05:35 -0300

    Add withdraw and deposit functions for balance management

--- functions.rs
+++ functions.rs
@@ -5,3 +5,21 @@ pub fn another_function(x: i32) {
 pub fn print_labbled_measurement(value: i32, unit_label: char) {
     println!("The measurement is: {value}{unit_label}");
 }
+
+pub fn withdraw(amount: u32, balance: &mut u32) -> Result<(), String> {
+    if *balance < amount {
+        Err(String::from("Insufficient funds"))
+    } else {
+        *balance -= amount;
+        Ok(())
+    }
+}
+
+pub fn deposit(amount: u32, balance: &mut u32) -> Result<(), String> {
+    if amount <= 0 {
+        Err(String::from("Deposit amount must be positive"))
+    } else {
+        *balance += amount;
+        Ok(())
+    }
+}
